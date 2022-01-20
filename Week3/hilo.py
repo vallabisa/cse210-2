e250af4f44a7b58c6f3424111a562e6c2bae5540
dragonslayer109 <raydonvetten@gmail.com>
2022-01-20 16:34:08 +0200

    Update hilo.py
    Initial functionality added and methods added to program

--- Week3/hilo.py
+++ Week3/hilo.py
@@ -2,8 +2,30 @@ class hilo:
     pass
 
 class director:
-    pass
+    def __init__(self):
+        
+        self.score = 300
+        self.cards = []
+        self.is_playing = True
+        
+    def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.is_playing:
+            self.do_updates()
+            self.get_prompt()
 
+    def get_prompt(self):
+        print(f"The card is: {self.cards}")
+        card_guess = input("Higher or lower? [h/1] ")
+
+    def do_updates(self):
+        if not self.is_playing:
+            return 
+    
 def main():
     director = Director()
     director.start_game()
