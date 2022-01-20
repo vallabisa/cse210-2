78ddece224615e694e8168251d0c975d09892201
dragonslayer109 <raydonvetten@gmail.com>
2022-01-20 17:12:40 +0200

    Update hilo.py

--- Week3/hilo.py
+++ Week3/hilo.py
@@ -1,17 +1,21 @@
-class hilo:
+import random
+class Hilo:
     def __init__(self):
-        self.value = 0
+        self.value = random.randint(1, 13)
         self.points = 0
 
-    
+    def card_draw(self):
+        if self.value == 
 
-class director:
+class Director:
     def __init__(self):
         
         self.score = 300
         self.total_score = 0
         self.cards = []
         self.is_playing = True
+        self.card = Hilo()
+        self.cards.append(self.card)
         
     def start_game(self):
         """Starts the game by running the main game loop.
@@ -25,18 +29,29 @@ class director:
 
 
     def get_prompt(self):
-        print(f"The card is: {self.cards}")
-        card_guess = input("Higher or lower? [h/1] ")
-        print(f"Next card is: {self.cards}")
+        i = 0
+        j = 1
+        print(f"The card is: {self.card.value[i]}")
+        card_guess = input("Higher or lower? [h/l] ")
+        self.cards.append(self.card)
+        print(f"Next card is: {self.card.value[j]}")
         print(self.total_score)
         run_again = input("Play again? [y/n]")
+        if run_again == "y":
+            i += 1
+            j += 1
         self.is_playing = (run_again == "y")
 
 
     def do_updates(self):
         if not self.is_playing:
             return 
-    
+        
+        card = self.cards[]
+        card.card_draw()
+        self.score += card.points 
+        self.total_score += self.score
+
 def main():
     director = Director()
     director.start_game()
