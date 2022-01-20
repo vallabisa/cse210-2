161a3bea0f73616e582063936209e19c136a9a25
dragonslayer109 <raydonvetten@gmail.com>
2022-01-20 16:42:17 +0200

    Update hilo.py
    More logic added to director class with methods. hilo class init added

--- Week3/hilo.py
+++ Week3/hilo.py
@@ -1,10 +1,15 @@
 class hilo:
-    pass
+    def __init__(self):
+        self.value = 0
+        self.points = 0
+
+    
 
 class director:
     def __init__(self):
         
         self.score = 300
+        self.total_score = 0
         self.cards = []
         self.is_playing = True
         
@@ -15,12 +20,18 @@ class director:
             self (Director): an instance of Director.
         """
         while self.is_playing:
-            self.do_updates()
             self.get_prompt()
+            self.do_updates()
+
 
     def get_prompt(self):
         print(f"The card is: {self.cards}")
         card_guess = input("Higher or lower? [h/1] ")
+        print(f"Next card is: {self.cards}")
+        print(self.total_score)
+        run_again = input("Play again? [y/n]")
+        self.is_playing = (run_again == "y")
+
 
     def do_updates(self):
         if not self.is_playing:
